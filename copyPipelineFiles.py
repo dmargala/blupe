4b5a69ced5cc70dbaad147eebb72c1b4fed85382
Daniel Margala <dmargala@ln000.(none)>
2013-04-04 11:40:27 -0700

    First commit! Add scripts to run 'blue' pipeline and plot reduced products

---
+++ copyPipelineFiles.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#
+# Created 12-Mar-2013 by Daniel Margala (University of California, Irvine) <dmargala@uci.edu>
+#
+# usage: 
+
+# Import libraries
+import os
+import shutil
+import fnmatch
+import subprocess
+
+# Set up appropriate paths
+origBSRPath = '/clusterfs/riemann/raid006/bosswork/boss/spectro/redux/v5_6_0'
+newBSRPath = '/clusterfs/riemann/raid006/bosswork/boss/spectro/redux/test/dmargala/redux/v5_6_0'
+
+force = False
+
+def copyPipelineFiles():
+	# Read in list of plates we are interested in
+	for line in open('bluePlateMJDList.txt','r'):
+		(plate, mjd) = line.strip().split()
+		origDir = os.path.join(origBSRPath,plate)
+		newDir = os.path.join(newBSRPath,plate)
+		patterns = ['spFlat*','spArc*','spFrame*','photo*','*.par','redux-%s-%s'%(plate,mjd)]
+		#patterns = ['*.par','redux-%s-%s'%(plate,mjd)]
+		if not os.path.exists(newDir):
+			os.makedirs(newDir)
+		for filename in os.listdir(origDir):
+			if any(fnmatch.fnmatch(filename, p) for p in patterns):
+				if not force and os.path.isfile(os.path.join(newDir,filename)):
+					print 'The following file already exists, please remove it or run with force to continue:'
+					print os.path.join(newDir,filename)
+					continue
+				shutil.copy(os.path.join(origDir,filename),os.path.join(newDir,filename)) 
+		os.chdir(newDir)
+		reduxfilename = 'redux-%s-%s'%(plate,mjd)
+		reduxfile = open(reduxfilename,'r')
+		output = []
+		for reduxline in reduxfile:
+	   		if not "setup" in reduxline:
+	        		output.append(reduxline)
+			else:
+				output.append('export SPECLOG_DIR=/clusterfs/riemann/raid006/bosswork/boss/spectro/redux/test/dmargala/speclog\n')
+		reduxfile.close()
+		reduxfile = open(reduxfilename, 'w')
+		reduxfile.writelines(output)
+		reduxfile.close()
+
+
+		subprocess.call(['pwd'])
+		subprocess.call(['echo','qsub','redux-%s-%s'%(plate,mjd)])
+		subprocess.call(['qsub','redux-%s-%s'%(plate,mjd)])
+
+if __name__ == '__main__':
+	copyPipelineFiles()
