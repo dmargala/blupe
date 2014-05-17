8a62e4ea024e0ca4b844c03df0b07deeaca9ffdd
Daniel Margala <dmargala@uci.edu>
2014-05-16 17:42:51 -0700

    clean up copyPipelineFiles script

--- copyPipelineFiles.py
+++ copyPipelineFiles.py
@@ -9,60 +9,71 @@ import os
 import shutil
 import fnmatch
 import subprocess
+import argparse
 
-# Set up appropriate paths
-origBSRPath = '/clusterfs/riemann/raid008/bosswork/boss/spectro/redux/v5_7_0'
-newBSRPath = '/clusterfs/riemann/raid008/bosswork/boss/spectro/redux/test/dmargala/redux/v5_7_0'
-speclog_dir = '/clusterfs/riemann/raid008/bosswork/boss/spectro/redux/test/dmargala/speclog'
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-v","--verbose", action = "store_true",
+        help = "provide more verbose output")
+    parser.add_argument("-i","--input", type=str, default="bluePlateMJDList.txt",
+        help = "input list plate-mjd pairs to copy")
+    parser.add_argument("--clobber", action = "store_true",
+        help = "overwrite existing files")
+    parser.add_argument("--speclog", type=str, default="/clusterfs/riemann/raid008/bosswork/boss/spectro/redux/test/dmargala/speclog",
+        help = "speclog dir containing modified plugmap files")
+    parser.add_argument("--bsr-orig", type=str, default="/clusterfs/riemann/raid008/bosswork/boss/spectro/redux",
+        help = "path to original reduction")
+    parser.add_argument("--bsr-new", type=str, default="/clusterfs/riemann/raid008/bosswork/boss/spectro/redux/test/dmargala/redux",
+        help = "path to new reduction")
+    parser.add_argument("--run2d", type=str, default="v5_7_0",
+        help = "run2d version")
+    parser.add_argument("--qsub", action="store_true",
+        help = "submit jobs to queue")
+    args = parser.parse_args()
 
-force = False
+    origDir = os.path.join(args.bsr_orig,args.run2d)
+    newDir = os.path.join(args.bsr_new,args.run2d)
+    if args.verbose:
+        print 'Creating mirror of %s at %s' % (origDir, newDir)
 
-def copyPipelineFiles():
-    # Read in list of plates we are interested in
-    for line in open('bluePlateMJDList.txt','r'):
+    # read in list of plates to copy
+    for line in open(args.input,'r'):
         (plate, mjd) = line.strip().split()
-        origDir = os.path.join(origBSRPath,plate)
-        newDir = os.path.join(newBSRPath,plate)
+        origDirPlate = os.path.join(origDir,plate)
+        newDirPlate = os.path.join(newDir,plate)
+        # file patterns to copy over to new working directory
         patterns = ['spFlat*','spArc*','spFrame*','photo*','*.par','redux-%s-%s'%(plate,mjd)]
-        #patterns = ['*.par','redux-%s-%s'%(plate,mjd)]
-        if not os.path.exists(newDir):
-            os.makedirs(newDir)
-        for filename in os.listdir(origDir):
+        if not os.path.exists(newDirPlate):
+            os.makedirs(newDirPlate)
+        for filename in os.listdir(origDirPlate):
             if any(fnmatch.fnmatch(filename, p) for p in patterns):
-                if not force and os.path.isfile(os.path.join(newDir,filename)):
-                    #print 'The following file already exists, please remove it or run with force to continue:'
-                    #print os.path.join(newDir,filename)
+                if not args.clobber and os.path.isfile(os.path.join(newDirPlate,filename)):
+                    print 'The following file already exists, please remove it or run with --clobber to continue:'
+                    print os.path.join(newDirPlate,filename)
                     continue
-                print os.path.join(newDir,filename)
-                shutil.copy(os.path.join(origDir,filename),os.path.join(newDir,filename)) 
-        os.chdir(newDir)
+                print os.path.join(newDirPlate,filename)
+                shutil.copy(os.path.join(origDirPlate,filename),os.path.join(newDirPlate,filename)) 
+        # change into working directory
+        os.chdir(newDirPlate)
+        # Modify the submission script so it doesn't overwrite custom env variables
         reduxfilename = 'redux-%s-%s'%(plate,mjd)
         reduxfile = open(reduxfilename,'r')
         output = []
-
         for reduxline in reduxfile:
+            output.append(reduxline)
             if "setup" in reduxline:
-                output.append(reduxline)
                 output.append('export SPECLOG_DIR=%s\n'%speclog_dir)
-            elif 'v5_6_0' in reduxline:
-                parts = reduxline.split("\"")
-                newparts = []
-                for part in parts:
-                    if part == 'v5_6_0':
-                        newparts.append("v5_6_5")
-                    else:
-                        newparts.append(part)
-                output.append("\"".join(newparts))
-            else:
-                output.append(reduxline)
         reduxfile.close()
         reduxfile = open(reduxfilename, 'w')
         reduxfile.writelines(output)
         reduxfile.close()
-
-        subprocess.call(['pwd'])
-        subprocess.call(['echo','qsub','-q','batch','redux-%s-%s'%(plate,mjd)])
-        #subprocess.call(['qsub','-q','batch','redux-%s-%s'%(plate,mjd)])
+        # print working directory and qsub command
+        if args.verbose:
+            subprocess.call(['pwd'])
+            subprocess.call(['echo','qsub','-q','batch','redux-%s-%s'%(plate,mjd)])
+        # submit job if specified
+        if args.qsub:
+            subprocess.call(['qsub','-q','batch','redux-%s-%s'%(plate,mjd)])
 
 if __name__ == '__main__':
-    copyPipelineFiles()
+    main()
